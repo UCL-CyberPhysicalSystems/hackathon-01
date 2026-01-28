7e5dfd7f09755bbdaab7589e0b8db4f4457d7de9
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2026-01-28 21:13:43 +0000

    solves XDG_RUNTIME_DIR not set, defaulting to `/tmp/runtime-root` passing rght devide and runtime paths

--- ros2/cuda/run-ros2cuda.bash
+++ ros2/cuda/run-ros2cuda.bash
@@ -7,12 +7,11 @@ docker run --name $1 --entrypoint bash -it --rm --runtime=nvidia --gpus all \
     --network=host \
     --ipc=host \
     -e "PRIVACY_CONSENT=Y" \
+    --device /dev/dri \
+    -e XDG_RUNTIME_DIR=/tmp/runtime-root \
     -e "DISPLAY=$DISPLAY" \
     -e "QT_X11_NO_MITSHM=1" \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v $HOME/.Xauthority:/root/.Xauthority:rw \
     -v $HOME/repositories/UCL-CyberPhysicalSystems/hackathon-01:/home/hackathon-01 \
     $IMAGE_NAME
-
-
-    #-v "$HOME/.Xauthority:/home/rosuser/.Xauthority:rw" \
